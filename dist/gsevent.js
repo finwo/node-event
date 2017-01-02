9409f3929969cd72b2676f0bd1e0169b8b969624
Robin Bron <robin@finwo.nl>
2017-01-02 16:44:42 +0100

    Added support for global & multi-named listeners

--- dist/gsevent.js
+++ dist/gsevent.js
@@ -1,8 +1,12 @@
 // Browser
 (function ( exports ) {
-  var registeredListeners = {};
+  var registeredListeners = {},
+      globalListeners     = [];
   var eventSimulator      = function ( name, data ) {
     data = data || true;
+    globalListeners.forEach(function ( callback ) {
+      data = data && callback.call(null, data, name);
+    });
     if ( registeredListeners[ name ] ) {
       registeredListeners[ name ].forEach(function ( callback ) {
         data = data && callback.call(null, data);
@@ -11,6 +15,16 @@
     return data;
   };
   eventSimulator.on       = function ( name, callback ) {
+    if ( typeof name === 'function' ) {
+      globalListeners.push(name);
+      return eventSimulator;
+    }
+    if ( name.forEach ) {
+      name.forEach(function ( currentName ) {
+        eventSimulator.on(currentName, callback);
+      });
+      return eventSimulator;
+    }
     registeredListeners[ name ] = registeredListeners[ name ] || [];
     registeredListeners[ name ].push(callback);
     return eventSimulator;
